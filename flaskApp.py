926a767e106028d164fabd1a22a82e9be779f3cc
edmond120 <edmondwong888@msn.com>
2017-09-26 14:48:20 -0400

    tweaked some stuff

--- flaskApp.py
+++ flaskApp.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
+from occ import toDictionary, pickOccupation
 
 app = Flask(__name__)
 
 #render_template(<file>,<var1>,<var2>,...) returns a html template file as a string
+def dicToTable(dic):
+    
 
 @app.route("/occupations")
 def occupations():
     occ_table = ''
-    return render_template('tableTemp.html',table=occ_table)
+    dic = toDictionary("occupations.csv")
+    pickedOcc = pickOccupation(dic)
+    occ_table = dicToTable(dic)
+    return render_template('tableTemp.html',table=occ_table,occupation=pickedOcc)
 
 
 if __name__ == '__main__':
