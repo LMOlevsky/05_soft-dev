e485bb8add968729ba216d92fd9e1a7afd13581a
edmond120 <edmondwong888@msn.com>
2017-09-26 14:42:53 -0400

    edited flaskApp

--- flaskApp.py
+++ flaskApp.py
@@ -4,9 +4,10 @@ app = Flask(__name__)
 
 #render_template(<file>,<var1>,<var2>,...) returns a html template file as a string
 
-@app.route("/")
-def root():
-    return render_template('base1.html')
+@app.route("/occupations")
+def occupations():
+    occ_table = ''
+    return render_template('tableTemp.html',table=occ_table)
 
 
 if __name__ == '__main__':
