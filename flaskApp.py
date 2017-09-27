da2ec10ebeb92500ea47016cf5d41b22c9bdef47
edmond120 <edmondwong888@msn.com>
2017-09-26 21:34:28 -0400

    bugs

--- flaskApp.py
+++ flaskApp.py
@@ -5,13 +5,18 @@ app = Flask(__name__)
 
 #render_template(<file>,<var1>,<var2>,...) returns a html template file as a string
 def dicToTable(dic):
-    
+    table = '<table style="width:100%"><tr><th>Job Class</th><th>Percentage</th></th>'
+    for key in dic:
+        if(not(key == "Job Class")):
+            table += "<tr><td>" + key + "<\td><td>" + str(dic[key]) + "</td></tr>"
+    table += "</table>"
+    return table
 
 @app.route("/occupations")
 def occupations():
     occ_table = ''
     dic = toDictionary("occupations.csv")
-    pickedOcc = pickOccupation(dic)
+    pickedOcc = "<h3>" + pickOccupation(dic) + "</h3>"
     occ_table = dicToTable(dic)
     return render_template('tableTemp.html',table=occ_table,occupation=pickedOcc)
 
